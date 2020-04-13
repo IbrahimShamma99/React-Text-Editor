9c9f8d9174ab06022863b835d6676b0fe1b6ed84
ibrahimshamma99 <i.abushammah@gmail.com>
2020-04-13 16:49:52 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import PageContainer from './components/PageContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PageContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PageContainer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Editor, EditorState, RichUtils } from 'draft-js';
+
+class PageContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    }
+  }
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState 
+    }) 
+  }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+
+  onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+
+  onItalicClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+  }
+
+
+  render() {
+    return(
+      <div className="editorContainer">
+        <button onClick={this.onUnderlineClick}>U</button>
+        <button onClick={this.onBoldClick}><b>B</b></button>
+        <button onClick={this.onItalicClick}><em>I</em></button>        <div className="editors">
+        <Editor 
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange= { this.onChange }
+          />
+          </div>
+      </div>
+    )
+  }
+}
+
+export default PageContainer
