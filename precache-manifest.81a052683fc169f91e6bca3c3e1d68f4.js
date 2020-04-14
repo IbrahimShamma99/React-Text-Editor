6e18280378b2b6796276d8763be64ef7bdfdfc10
ibrahimshamma99 <i.abushammah@gmail.com>
2020-04-14 03:50:21 +0300

    Updates

---
+++ precache-manifest.81a052683fc169f91e6bca3c3e1d68f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d2a9f67ce8af35e79411adba669909",
+    "url": "/React-Text-Editor/index.html"
+  },
+  {
+    "revision": "619093c2d158a509709f",
+    "url": "/React-Text-Editor/static/css/2.260df227.chunk.css"
+  },
+  {
+    "revision": "639d5a1552f8ac189830",
+    "url": "/React-Text-Editor/static/css/main.e23e5144.chunk.css"
+  },
+  {
+    "revision": "619093c2d158a509709f",
+    "url": "/React-Text-Editor/static/js/2.e5f249c1.chunk.js"
+  },
+  {
+    "revision": "bb31799dc6b58ea6d9d7ac07fc13b2f1",
+    "url": "/React-Text-Editor/static/js/2.e5f249c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639d5a1552f8ac189830",
+    "url": "/React-Text-Editor/static/js/main.3ac5a55c.chunk.js"
+  },
+  {
+    "revision": "e72cc489cd15640a76f5",
+    "url": "/React-Text-Editor/static/js/runtime-main.006f9813.js"
+  }
+]);
